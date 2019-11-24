ada77c7bb5970982b44b03d1a2ec6338ea4c881a
Luis Flosi <54824168+LuisFlosi@users.noreply.github.com>
2019-11-23 18:00:00 -0600

    Joined upto2018 and from2018; created region aggregation by lookup table; optimized column creation

--- Cleaning_Code/cleaning_traffic_from2018.py
+++ Cleaning_Code/cleaning_traffic_from2018.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 
-traffic_df = pd.read_csv("Chicago_Traffic_2018-2019.csv")
+traffic_from2018_df = pd.read_csv("traffic_from_2018.csv")
 
 # Change names
-traffic_df.columns = ["timestamp_id",
+traffic_from2018_df.columns = ["timestamp_id",
                       "segment_id",
                       "speed",
                       "street",
@@ -27,7 +27,7 @@ traffic_df.columns = ["timestamp_id",
                       "end_location"]
 
 # Remove columns
-traffic_df.drop(["street",
+traffic_from2018_df.drop(["street",
                   "direction",
                   "from_street",
                   "to_street",
@@ -40,31 +40,30 @@ traffic_df.drop(["street",
                   "start_latitude",
                   "start_longitude",
                   "end_latitude",
-                  "end_longitude",
-                  "start_location",
-                  "end_location"],
+                  "end_longitude"],
                  axis = 1)
-
-# Change datetime
-traffic_df.timestamp_id = pd.to_datetime(traffic_df.timestamp_id)
 
 # Filter out dates
 min_date = '2018-02-28'
 max_date = '2018-10-31'
-traffic_df = traffic_df[(traffic_df['timestamp_id'] >= min_date) & (traffic_df['timestamp_id'] <= max_date)]
+traffic_from2018_df = traffic_from2018_df[(traffic_from2018_df['timestamp_id'] >= min_date) &
+                        (traffic_from2018_df['timestamp_id'] <= max_date)]
+
+# Change timestamp to datetime
+traffic_from2018_df.timestamp_id = pd.to_datetime(traffic_from2018_df.timestamp_id)
 
 # Aggregate timestamp by hour
-traffic_df = traffic_df.set_index('timestamp_id')
+traffic_from2018_df = traffic_from2018_df.set_index('timestamp_id')
 
-traffic_df = traffic_df.groupby(["segment_id"]).resample("H").agg({
-        'bus_count' : 'sum',
-        'gps_pings' : 'sum',
-        'speed' : 'mean'}).ffill()
+traffic_from2018_df = traffic_from2018_df.groupby(["segment_id"]).resample("H").agg({
+                                                    'bus_count' : 'sum',
+                                                    'gps_pings' : 'sum',
+                                                    'speed' : 'mean'}).ffill()
 
-traffic_df = traffic_df.reset_index()
+traffic_from2018_df = traffic_from2018_df.reset_index()
 
 # Fill missing hours
-filled_df = (traffic_df.set_index('timestamp_id')
+filled_df = (traffic_from2018_df.set_index('timestamp_id')
              .groupby('segment_id')
              .apply(lambda d: d.reindex(pd.date_range(min(traffic_df.timestamp_id),
                                                       max(traffic_df.timestamp_id),
@@ -78,10 +77,92 @@ fill_values = {"bus_count" : 0, "gps_pings" : 0, "speed" : -1}
 
 filled_df = filled_df.fillna(value = fill_values)
 
-# Add speed category and comments columns
+# Reorder columns
+filled_df["timestamp_id"] = filled_df.index
+filled_df = filled_df.reset_index()
+traffic_from2018 = filled_df[["segment_id",
+                              "timestamp_id",
+                              "speed",
+                              "bus_count",
+                              "gps_pings"]]
+
+# Load upto2018
+
+traffic_upto2018_df = pd.read_csv("traffic_upto_2018.csv")
+
+# Change column names
+
+traffic_upto2018_df.columns = ["timestamp_id",
+                               "segment_id",
+                               "bus_count",
+                               "gps_pings",
+                               "speed"]
+
+# Change datetime
+
+traffic_upto2018_df.timestamp_id = pd.to_datetime(traffic_upto2018_df.timestamp_id)
+
+# Filter out dates
+min_date = '2014-01-01'
+max_date = '2018-02-27'
+traffic_upto2018_df = traffic_upto2018_df[(traffic_upto2018_df['timestamp_id'] >= min_date) &
+                                          (traffic_upto2018_df['timestamp_id'] <= max_date)]
+
+# Aggregate timestamp by hour
+
+traffic_upto2018_df = traffic_upto2018_df.set_index('timestamp_id')
+
+traffic_upto2018_df = traffic_upto2018_df.groupby(["segment_id"]).resample("H").agg({
+                                                                'bus_count' : 'sum',
+                                                                'gps_pings' : 'sum',
+                                                                'speed' : 'mean'}).ffill()
+
+traffic_upto2018_df = traffic_upto2018_df.reset_index()
+
+# Fill missing hours
+
+filled_df = (traffic_upto2018_df.set_index('timestamp_id')
+             .groupby('segment_id')
+             .apply(lambda d: d.reindex(pd.date_range(min(traffic_upto2018_df.timestamp_id),
+                                                      max(traffic_upto2018_df.timestamp_id),
+                                                      freq='H')))
+             .drop('segment_id', axis=1)
+             .reset_index('segment_id')
+             .groupby('segment_id')
+             .ffill())
+             
+traffic_upto2018 = filled_df.fillna(value = fill_values)
+
+# Reorder columns
+filled_df["timestamp_id"] = filled_df.index
+filled_df = filled_df.reset_index()
+filled_df = filled_df[["segment_id","timestamp_id","speed","bus_count","gps_pings"]]
+
+# Join with upto2018
+
+traffic_full = pd.concat([traffic_upto2018,traffic_from2018])
+
+# Insert region information for segments
+
+region_lookup_df = pd.read_csv("segment_region_lookup.csv")
+
+traffic_full = pd.merge(traffic_full,
+                      region_lookup_df["region"],
+                      on = "segment_id",
+                      how = "left")
+
+traffic_full = traffic_full.drop("segment_id")
+
+# Regroup by region
+
+agg_dic = {"bus_count" : "sum", "gps_pings" : "sum", "speed" : "max"}
+
+traffic_full = traffic_full.groupby(['region','timestamp_id']).agg(agg_dic).ffill()
+
+# Add speed category
 def speed_condition(c):
   if c['speed'] == -1:
-      return ''
+      return 'unavailable'
   elif c['speed'] < 10:
     return 'slow'
   elif 10 <= c['speed'] < 20:
@@ -91,25 +172,14 @@ def speed_condition(c):
   else:
     return 'undefined'
 
-filled_df["speed_category"] = filled_df.apply(speed_condition, axis = 1)
-
-# Reorder columns
-filled_df["timestamp_id"] = filled_df.index
-filled_df = filled_df.reset_index()
-traffic_from2018 = filled_df[["segment_id","timestamp_id","speed","speed_category","bus_count","gps_pings"]]
-
-
-# Join with upto2018
-traffic_upto2018 = pd.read.csv("Cleaned_Chicago_Traffic_2011-2018.csv")
-
-traffic_full = pd.concat([traffic_upto2018,traffic_from2018])
+traffic_full["speed_category"] = traffic_full.apply(speed_condition, axis = 1)
 
 # create traffic unique id
-traffic_full["traffic_id"] = traffic_full.timestamp_id.strftime('%Y-%m-%d %H') + str(traffic_full.segment_id)
+traffic_full["traffic_id"] = traffic_full.timestamp_id.strftime('%Y-%m-%d %H') + str(traffic_full.region)
 
 # Export final traffic data
 
-traffic_full.to_csv(r"full_traffic_data.csv")
+traffic_full.to_csv(r"clean_full_traffic_data.csv")
 
 # Export to bucket
 
@@ -127,4 +197,4 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         source_file_name,
         destination_blob_name))
     
-upload_blob("rideshare-csvs", "full_traffic_data.csv", "rideshare_data")
+upload_blob("rideshare-csvs", "full_traffic_data.csv", "rideshare_bucket")
