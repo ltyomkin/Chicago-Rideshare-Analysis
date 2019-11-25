771640657b5964732da0f15fcd6607ca1f11fbac
Luis Flosi <54824168+LuisFlosi@users.noreply.github.com>
2019-11-25 07:21:12 -0600

    Removed old file

--- Cleaning_Code/taxi_rideshare_merge.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-taxi_cleaned = pd.read_csv("~/clean_taxi_trips.csv")
-rideshare_cleaned = pd.read.csv("~/clean_rideshare_trips.csv")
-
-trips_final = taxi_cleaned.merge(rideshare_cleaned, on = "trip_id")
-trips_final.to_csv(r"~/merged_taxi_rideshare.csv")
