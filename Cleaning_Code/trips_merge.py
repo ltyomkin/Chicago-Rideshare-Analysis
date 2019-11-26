f8d72eac0cef72dff9f6312ee9f1c2208f4c4f5d
Luis Flosi <luisflosi@uchicago.edu>
2019-11-26 08:16:09 -0500

    Fixed indexing in trips_merge

--- Cleaning_Code/trips_merge.py
+++ Cleaning_Code/trips_merge.py
@@ -3,7 +3,7 @@ import pandas as pd
 taxi_cleaned = pd.read_csv("~/clean_taxi_trips.csv")
 rideshare_cleaned = pd.read_csv("~/clean_rideshare_trips.csv")
 
-trips_final = pd.concat((taxi_cleaned,rideshare_cleaned))
+trips_final = pd.concat((taxi_cleaned,rideshare_cleaned), ignore_index = True)
 
 trips_final = trips_final.drop(["Unnamed: 0", "start_census_tract", "end_census_tract"], axis = 1, errors = "ignore")
 
