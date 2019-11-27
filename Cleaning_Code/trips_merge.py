b0a2711477e8cb82886435afc5faffa0f27ac76f
Luis Flosi <luisflosi@uchicago.edu>
2019-11-26 23:47:04 -0500

    Inplace for merge

--- Cleaning_Code/trips_merge.py
+++ Cleaning_Code/trips_merge.py
@@ -5,6 +5,6 @@ rideshare_cleaned = pd.read_csv("~/clean_rideshare_trips.csv")
 
 trips_final = pd.concat((taxi_cleaned,rideshare_cleaned), ignore_index = True)
 
-trips_final = trips_final.drop(["Unnamed: 0", "start_census_tract", "end_census_tract"], axis = 1, errors = "ignore")
+trips_final = trips_final.drop(["Unnamed: 0", "start_census_tract", "end_census_tract"], axis = 1, errors = "ignore", inplace = True)
 
 trips_final.to_csv(r"~/cleaned_full_trips.csv")
