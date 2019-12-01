519fd12886f835bf6c27e0541a6f85d81948974f
Luis Flosi <luisflosi@uchicago.edu>
2019-11-30 18:33:57 -0600

    replaced schema name

--- Data_Warehouse/final_DDL.sql
+++ Data_Warehouse/final_DDL.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema rideshare_snowflake
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema rideshare_snowflake
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `rideshare_snowflake` DEFAULT CHARACTER SET utf8 ;
+USE `rideshare_snowflake` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`dim_time`
+-- Table `rideshare_snowflake`.`dim_time`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`dim_time` (
+CREATE TABLE IF NOT EXISTS `rideshare_snowflake`.`dim_time` (
   `timestamp_id` TIMESTAMP NOT NULL,
   `time` TIME NULL,
   `date` DATE NULL,
@@ -29,9 +29,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`dim_base_region`
+-- Table `rideshare_snowflake`.`dim_base_region`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`dim_base_region` (
+CREATE TABLE IF NOT EXISTS `rideshare_snowflake`.`dim_base_region` (
   `base_region_id` INT(3) NOT NULL,
   `region_name` VARCHAR(45) NULL,
   `latitude` FLOAT NULL,
@@ -41,9 +41,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`dim_trip_region`
+-- Table `rideshare_snowflake`.`dim_trip_region`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`dim_trip_region` (
+CREATE TABLE IF NOT EXISTS `rideshare_snowflake`.`dim_trip_region` (
   `trip_region_id` INT(5) NULL,
   `base_region_id` INT(3) NULL,
   `trip_centroids` VARCHAR(45) NOT NULL,
@@ -51,16 +51,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`dim_trip_region` (
   INDEX `base_region_id_idx` (`base_region_id` ASC),
   CONSTRAINT `base_region_id`
     FOREIGN KEY (`base_region_id`)
-    REFERENCES `mydb`.`dim_base_region` (`base_region_id`)
+    REFERENCES `rideshare_snowflake`.`dim_base_region` (`base_region_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`trips`
+-- Table `rideshare_snowflake`.`trips`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`trips` (
+CREATE TABLE IF NOT EXISTS `rideshare_snowflake`.`trips` (
   `trip_id` INT(20) NOT NULL,
   `ride_type_id` INT(2) NOT NULL,
   `start_timestamp_id` TIMESTAMP NOT NULL,
@@ -82,31 +82,31 @@ CREATE TABLE IF NOT EXISTS `mydb`.`trips` (
   INDEX `dropoff_centroid_location_idx` (`dropoff_centroid_location` ASC),
   CONSTRAINT `start_timestamp_id`
     FOREIGN KEY (`start_timestamp_id`)
-    REFERENCES `mydb`.`dim_time` (`timestamp_id`)
+    REFERENCES `rideshare_snowflake`.`dim_time` (`timestamp_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `end_timestamp_id`
     FOREIGN KEY (`end_timestamp_id`)
-    REFERENCES `mydb`.`dim_time` (`timestamp_id`)
+    REFERENCES `rideshare_snowflake`.`dim_time` (`timestamp_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `pickup_centroid_location`
     FOREIGN KEY (`pickup_centroid_location`)
-    REFERENCES `mydb`.`dim_trip_region` (`trip_centroids`)
+    REFERENCES `rideshare_snowflake`.`dim_trip_region` (`trip_centroids`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `dropoff_centroid_location`
     FOREIGN KEY (`dropoff_centroid_location`)
-    REFERENCES `mydb`.`dim_trip_region` (`trip_centroids`)
+    REFERENCES `rideshare_snowflake`.`dim_trip_region` (`trip_centroids`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`dim_traffic`
+-- Table `rideshare_snowflake`.`dim_traffic`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`dim_traffic` (
+CREATE TABLE IF NOT EXISTS `rideshare_snowflake`.`dim_traffic` (
   `traffic_id` VARCHAR(45) NOT NULL,
   `region_id` INT(12) NOT NULL,
   `timestamp_id` TIMESTAMP NOT NULL,
@@ -119,12 +119,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`dim_traffic` (
   INDEX `timestamp_id_idx` (`timestamp_id` ASC),
   CONSTRAINT `region_id`
     FOREIGN KEY (`region_id`)
-    REFERENCES `mydb`.`dim_base_region` (`base_region_id`)
+    REFERENCES `rideshare_snowflake`.`dim_base_region` (`base_region_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `timestamp_id`
     FOREIGN KEY (`timestamp_id`)
-    REFERENCES `mydb`.`dim_time` (`timestamp_id`)
+    REFERENCES `rideshare_snowflake`.`dim_time` (`timestamp_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
